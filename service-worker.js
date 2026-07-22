52205a7d417a2f93a15cb4e31dc19e6a2ab48c6e
Sentury Status <senturystatus@gmail.com>
2026-07-22 13:35:31 -0400

    StatusOS v0.1.0 dashboard

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "statusos-v3.7.0";
+const CACHE = "statusos-v3.7.0-mobile-audio-fix-1";
 const CORE = [
   "./index.html", "./style.css", "./manifest.json", "./favicon.png",
   "./icon-192.png", "./icon-512.png", "./focus-planner.js", "./performance-timer.js",
